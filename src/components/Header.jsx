522ecc38db1ed54487a50e8e5be0b8dcfc93c741
Adrián Sanjuán Plaza <sanjuanplazaadrian@gmail.com>
2025-10-23 18:40:10 +0200

    Added the header and some features including the collapsable navbar and the light/dark mode button

---
+++ src/components/Header.jsx
@@ -0,0 +1,54 @@
+import { useContext, useState, useEffect } from "react";
+import { Navbar, Nav, Container, Button } from 'react-bootstrap';
+import { BsSun, BsMoon } from 'react-icons/bs';
+import logo_black from '../assets/logo_sin_fondo_negro.png'
+import logo_light from '../assets/logo_sin_fondo_blanco.png'
+import { MyContext } from '../App';
+
+function header() {
+
+    const { isDarkMode, setIsDarkMode } = useContext(MyContext)
+
+    function toggleTheme(){
+        if(isDarkMode){
+            setIsDarkMode(false)
+        }else{
+            setIsDarkMode(true)
+        }
+    }
+
+  return (
+    <Navbar bg={isDarkMode ? "dark" : "light"} variant={isDarkMode ? "dark" : "light"} expand="l">
+      
+      <Container fluid>
+        
+        <Navbar.Brand href="#home">
+            <img 
+            src={isDarkMode ? logo_light : logo_black}
+            alt="logo"
+            width='150' 
+            height='auto'/></Navbar.Brand>
+        
+        <div className="d-flex align-items-center justify-items-end">
+            <Button variant={isDarkMode ? "outline-light" : "outline-light"} onClick={toggleTheme} className="me-2">
+                {isDarkMode ? <BsSun size={20} /> : <BsMoon size={20} />}
+            </Button>
+        </div>
+
+        <Navbar.Toggle aria-controls="basic-navbar-nav" bg="dark" />
+        
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link href="#inicio">Inicio</Nav.Link>
+            <Nav.Link href="#productos">Productos</Nav.Link>
+            <Nav.Link href="#servicios">Servicios</Nav.Link>
+            <Nav.Link href="#contacto">Contacto</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+        
+      </Container>
+    </Navbar>
+  )
+}
+
+export default header
